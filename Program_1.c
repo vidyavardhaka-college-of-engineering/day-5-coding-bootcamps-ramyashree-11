87865cf6d4293346eeb1370258c59a18ae699828
RamyashreeS11 <>
2021-05-28 12:01:02 +0000

    Program_1.c no of vowels using strings

--- Program_1.c
+++ Program_1.c
@@ -5,9 +5,19 @@
 int main()
 {
 	char str[10];
-	scanf("%s", str);
-
+  int  i=0,v=0;
+  printf("Enter the string name\n");
+  scanf("%s", str);
+  while(str[i++]!='\0')
+  {
+    if(str[i]=='a'||str[i]=='e'||str[i]=='i'||str[i]=='o'||str[i]=='u')
+  {
+    v++;
+  }
+  }
+  printf("the number of vowels are %d",v);
 	//Write your code here
 
 	return 0;
 }
+
