8e259714d6ae21fe8df863ddd83eb2fe9a62d16a
RamyashreeS11 <>
2021-05-28 12:09:57 +0000

    Program_2.c reversing string I added header file string.h

--- Program_2.c
+++ Program_2.c
@@ -2,11 +2,25 @@
 //Input: vvce & Output: ecvv
 
 #include<stdio.h>
+#include<string.h>
 
 int main()
 {
-	char str[10];
+	char str[10],temp;
+  int i=0,j=0;
+  printf("Enter the string to be reversed");
 	scanf("%s", str);
+  i=0;
+  j=strlen(str)-1;
+  while(i<j)
+  {
+    temp=str[i];
+    str[i]=str[j];
+    str[j]=temp;
+    i++;
+    j--;
+  }
+  printf("The reversed string is %s",str);
 
 	//Write your code here
 
